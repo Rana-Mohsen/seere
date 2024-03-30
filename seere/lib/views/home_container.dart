838d38e70ed99a2100b5d5a2182a30f8f15688e3
Rana-3r2m <rana3r2m@gmail.com>
2024-03-30 21:58:42 +0200

    navigation bar

---
+++ seere/lib/views/home_container.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:seere/views/app_center_view.dart';
+import 'package:seere/views/home_view.dart';
+import 'package:seere/views/my_info_view.dart';
+
+class HomeContainer extends StatefulWidget {
+  const HomeContainer({super.key,});
+  @override
+  State<HomeContainer> createState() => _HomeContainerState();
+}
+
+class _HomeContainerState extends State<HomeContainer> {
+  int currentIndex = 0;
+  List<Widget> pages = [HomeView(), AppCenterView(), MyInfoView()];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: "Application Center"),
+          BottomNavigationBarItem(icon: Icon(Icons.person_outline_outlined), label: "My Info"),
+        ],
+        onTap: (index) {
+          currentIndex = index;
+          setState(() {});
+        },
+      ),
+    );
+  }
+}
