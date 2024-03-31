cc0f08e16a41254b92de9217e0a80cfd36c0f1e7
Rana-3r2m <rana3r2m@gmail.com>
2024-03-31 15:13:31 +0200

    change nav bar icons

--- lib/views/home_container.dart
+++ lib/views/home_container.dart
@@ -4,14 +4,20 @@ import 'package:seere/views/home_view.dart';
 import 'package:seere/views/my_info_view.dart';
 
 class HomeContainer extends StatefulWidget {
-  const HomeContainer({super.key,});
+  const HomeContainer({
+    super.key,
+  });
   @override
   State<HomeContainer> createState() => _HomeContainerState();
 }
 
 class _HomeContainerState extends State<HomeContainer> {
   int currentIndex = 0;
-  List<Widget> pages = [const HomeView(), const AppCenterView(), const MyInfoView()];
+  List<Widget> pages = [
+    const HomeView(),
+    const AppCenterView(),
+    const MyInfoView()
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,9 +25,15 @@ class _HomeContainerState extends State<HomeContainer> {
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: currentIndex,
         items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.star), label: "Application Center"),
-          BottomNavigationBarItem(icon: Icon(Icons.person_outline_outlined), label: "My Info"),
+          BottomNavigationBarItem(
+              icon: ImageIcon(AssetImage("assets/icons/home.png")),
+              label: "Home"),
+          BottomNavigationBarItem(
+              icon: ImageIcon(AssetImage("assets/icons/app_center.png")),
+              label: "Application Center"),
+          BottomNavigationBarItem(
+              icon: ImageIcon(AssetImage("assets/icons/my_info.png")),
+              label: "My Info"),
         ],
         onTap: (index) {
           currentIndex = index;
