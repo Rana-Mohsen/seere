25936fb2af8bccb413a9df0a81c672ecfb2a1d89
Rana-3r2m <rana3r2m@gmail.com>
2024-05-17 18:45:33 +0300

    adding live data icons

--- lib/views/live_data_view.dart
+++ lib/views/live_data_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:sizer/sizer.dart';
 
 class LiveData extends StatefulWidget {
   const LiveData({super.key});
@@ -12,8 +15,64 @@ class _LiveDataState extends State<LiveData> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("OBD live data"),
+        title: const Text("OBD live data"),
         centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  dataContainer(),
+                  dataContainer(),
+                  dataContainer(),
+                  dataContainer(),
+                  dataContainer(),
+                ],
+              ),
+              SvgPicture.asset("assets/images/live_data_car.svg"),
+            ],
+          ),
+          Row(
+            children: [],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget dataContainer() {
+    return Container(
+      padding: EdgeInsets.all(24),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          RichText(
+            text: TextSpan(
+              style:
+                  TextStyle(color: Colors.black, fontWeight: FontWeight.w600),
+              children: [
+                TextSpan(text: '0', style: TextStyle(fontSize: 16.sp)),
+                TextSpan(
+                    text: 'km/h',
+                    style: TextStyle(fontSize: 8.sp, color: Colors.black54)),
+              ],
+            ),
+          ),
+          Row(
+            children: [
+              SvgPicture.asset("assets/icons/live_data_current_speed.svg"),
+              Text("Current Speed",
+                  style: TextStyle(
+                    fontSize: 8.sp,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black54,
+                  )),
+            ],
+          )
+        ],
       ),
     );
   }
